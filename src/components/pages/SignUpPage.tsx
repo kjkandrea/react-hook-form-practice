263167a3449f85947a33a417efdba26397e3d6a7
kjkandrea <kjkandrea@gmail.com>
2023-01-03 14:17:38 +0900

    feat: 회원가입에 react-hook-form 바인딩

--- src/components/pages/SignUpPage.tsx
+++ src/components/pages/SignUpPage.tsx
@@ -1,5 +1,11 @@
 import Form from '@/components/forms/Form/Form';
-import {useState} from 'react';
+import {SubmitHandler, useForm} from 'react-hook-form';
+
+interface SignUpFormValues {
+  email: string;
+  password: string;
+  passwordRepeat: string;
+}
 
 /**
  * TODO
@@ -7,38 +13,33 @@ import {useState} from 'react';
  * password 는 RegisterPasswordFieldSet 개념으로 그룹핑 시도해보기
  */
 export default function SignUpPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+  const {register, handleSubmit} = useForm<SignUpFormValues>();
+  const onSubmit: SubmitHandler<SignUpFormValues> = data => console.log(data);
 
   return (
     <div>
       <h1>Sign Up</h1>
-      <Form onSubmit={e => e.preventDefault()}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Input
-          ref={inputElem => email || inputElem?.focus()}
           id="email"
           type="email"
           label="이메일"
           placeholder="example@email.com"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
+          {...register('email')}
         />
         <Form.Input
           id="password"
           type="password"
           label="비밀번호"
           placeholder="******"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
+          {...register('password')}
         />
         <Form.Input
           id="password-repeat"
           type="password"
           label="비밀번호 확인"
           placeholder="******"
-          value={repeatPassword}
-          onChange={e => setRepeatPassword(e.target.value)}
+          {...register('passwordRepeat')}
         />
         <Form.Button type="submit">가입하기</Form.Button>
       </Form>
