991a7e42771bc460301aa95a7204c239e8a43cb0
kjkandrea <kjkandrea@gmail.com>
2023-01-03 16:25:47 +0900

    feat: FormFieldSet 추가

--- src/components/forms/Form/Form.tsx
+++ src/components/forms/Form/Form.tsx
@@ -2,6 +2,7 @@ import FormInput from '@/components/forms/Form/FormInput';
 import FormButton from '@/components/forms/Form/FormButton';
 import {FormHTMLAttributes, ReactNode} from 'react';
 import EmailInput from '@/components/forms/fields/EmailInput';
+import FormFieldSet from '@/components/forms/Form/FormFieldSet';
 
 interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
   children: ReactNode;
@@ -14,5 +15,6 @@ function Form({children, ...rest}: FormProps) {
 Form.Input = FormInput;
 Form.Button = FormButton;
 Form.EmailInput = EmailInput;
+Form.FieldSet = FormFieldSet;
 
 export default Form;
---
+++ src/components/forms/Form/FormFieldSet.tsx
@@ -0,0 +1,19 @@
+import {FormHTMLAttributes, ReactNode} from 'react';
+import FormInput from '@/components/forms/Form/FormInput';
+
+interface FormFieldSetProps extends FormHTMLAttributes<HTMLFieldSetElement> {
+  children: ReactNode;
+  name: string;
+}
+
+function FormFieldSet({children, name, ...rest}: FormFieldSetProps) {
+  return (
+    <fieldset {...rest} name={name}>
+      {children}
+    </fieldset>
+  );
+}
+
+FormFieldSet.Input = FormInput;
+
+export default FormFieldSet;
